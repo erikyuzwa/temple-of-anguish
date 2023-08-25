1e196e302bb20ab9b7ab21ce8638a3b73c2a11da
Erik Yuzwa <erikyuzwa@gmail.com>
2023-08-24 18:28:50 -0600

    removing the classic code (#5)

--- app/scripts/item.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/* globals Game */
-Game.Item = function(properties) {
-    properties = properties || {};
-    // Call the dynamic glyph's construtor with our set of properties
-    Game.DynamicGlyph.call(this, properties);
-};
-// Make items inherit all the functionality from dynamic glyphs
-Game.Item.extend(Game.DynamicGlyph);
